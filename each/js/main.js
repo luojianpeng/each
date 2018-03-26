8279a32c52cf388c2bed4d856607a37463a15c1d
luojianpeng <704669025@qq.com>
2018-03-26 18:25:22 +0800

    each响应式布局

---
+++ each/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+
+  	items:1,
+  	autoplay:true,
+  	autoplayTimeout:1000,
+  	loop:true,
+  	autoplayHoverPause:true
+
+  });
+}); 
